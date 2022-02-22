b4d5d3895aef4e20530c62741da58f9fbdb1db7c
ZixuanShu <60206922+ZixuanShu@users.noreply.github.com>
2022-02-22 15:51:23 -0500

    put, get post, abort method added

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from email import message
+from flask import Flask,request
+from flask_restful import Api, Resource, reqparse, abort
+from numpy import require
 
 app = Flask(__name__)
 api = Api(app)
 
+datas = {
+    1: {"name": "Jen", "age":22, "gender": "female", "bloodtype": "AB", "height": 165, "weight": "50kg"},
+    2: {"name": "Max", "age": 25, "gender": "male", "bloodtype": "B", "height": 185, "weight": "70kg"},
+    3: {"name": "Larry", "age": 10, "gender": "male", "bloodtype": "O", "height": 105, "weight": "30kg"}
+}
+
+data_put_args = reqparse.RequestParser()
+data_put_args.add_argument("name", type = str, help = "name of the patient is requried", required = True) 
+data_put_args.add_argument("age", type = str, help = "age of the patient is requried", required = True) 
+data_put_args.add_argument("gender", type = str, help = "gender of the patient is requried")
+data_put_args.add_argument("bloodtype", type = str, help = "bloodtype of the patient is requried")
+data_put_args.add_argument("height", type = str, help = "height of the patient is requried")
+data_put_args.add_argument("weight", type = str, help = "weight of the patient is requried")
+
+def abort_if_no_id(pid):
+    if pid not in datas:
+        abort(404, message = "Patient id is not valid...")
+
 class PatientInfo(Resource):
-    def get(self):
-        return {"data": "blood pressure, height, weight, age"}
+    def get(self, pid):
+        abort_if_no_id(pid)
+        return datas[pid]
 
-api.add_resource(PatientInfo, "/patientinfo")
+    def post(self):
+        return {"data": "Posted"}
+
+    def put(self,pid):
+        args = data_put_args.parse_args()
+        datas[pid] = args
+        return datas[pid], 201
+    
+
+
+api.add_resource(PatientInfo, "/patientinfo/<int:pid>")
  
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE + "/patientinfo")
-
+#response = requests.put(BASE + "/patientinfo/4", {"name": "Geo", "age": 18, "gender": "male", "bloodtype": "O", "height": 175, "weight": "77kg"})
+#input()
+response = requests.get(BASE + "/patientinfo/6")
 print(response.json())
