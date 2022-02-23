b39f46714a8220b93b4c33b3f34858f7a9325e97
ZixuanShu <60206922+ZixuanShu@users.noreply.github.com>
2022-02-22 22:16:16 -0500

    defined port

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ api.add_resource(PatientInfo, "/patientinfo/<int:pid>")
  
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port= 8080, debug=True)
+    app.run(host = '0.0.0.0', port= 80, debug=True)
