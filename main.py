f30dbe66d306eb5156934e98ca6234d36f98572e
ZixuanShu <60206922+ZixuanShu@users.noreply.github.com>
2022-02-22 22:09:02 -0500

    defined port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask,request
 from flask_restful import Api, Resource, reqparse, abort
 
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -55,4 +56,4 @@ api.add_resource(PatientInfo, "/patientinfo/<int:pid>")
  
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 80, debug=True)
+    app.run(host = '0.0.0.0', port= 8080, debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = "http://127.0.0.1:5000"
+BASE = "http://0.0.0.0:80"
 
 response = requests.put(BASE + "/patientinfo/4", {"name": "Geo", "age": 18, "gender": "male", "bloodtype": "O", "height": 175, "weight": "77kg"})
 input()
